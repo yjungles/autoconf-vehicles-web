d4a0ed724a696680bad294fc64569871eeea9392
Yuri <yjungles@hotmail.com>
2026-03-09 06:39:11 -0300

    chore: configurar aplicação - configura TS - configura vite - configura pacotes - configura tailwind e shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const backendHost = new URL(env.VITE_BACKEND_BASE_URL).hostname
+
+  return {
+    plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    server: {
+      host: backendHost,
+      port: 5173,
+      strictPort: true,
+      allowedHosts: [backendHost],
+    },
+  }
 })
