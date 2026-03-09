baa4e8d77252112a5885c04b18d1e2279ece0e05
Yuri <yjungles@hotmail.com>
2026-03-09 06:40:45 -0300

    refactor: configurar favicon e títulos da aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import { queryClient } from './app/providers/query-client'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin, {
+  queryClient,
+})
 
 app.mount('#app')
