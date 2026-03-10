e18871deb334e30240ad3316c634865747d13012
Yuri <yjungles@hotmail.com>
2026-03-10 16:22:14 -0300

    chore: ajusta vitest.config.ts para build

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,14 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
+export default defineConfig(async (env) => {
+  const resolvedViteConfig = typeof viteConfig === 'function' ? viteConfig(env) : viteConfig
+
+  return mergeConfig(resolvedViteConfig, {
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  }),
-)
+  })
+})
